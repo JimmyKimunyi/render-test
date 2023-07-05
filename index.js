0587efbf18bf633c8080de8ceb3ffebb6f8d7fbf
JimmyKimunyi <jimmykimunyi@gmail.com>
2023-07-05 21:27:34 +0300

    update

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
-  console.log("Request 2");
+  console.log("Request no 2");
 });
 
 const generateId = () => {
