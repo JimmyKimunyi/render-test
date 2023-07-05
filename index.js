784fef077fae736f26e2dbb4c8be53800f61ae66
JimmyKimunyi <jimmykimunyi@gmail.com>
2023-07-05 21:51:28 +0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const requestLogger = (req, _res, next) => {
   console.log(" Method :  ", req.method);
   console.log(" Path : ", req.path);
   console.log(" Body : ", req.body);
-  console.log(" --- ");
+  console.log("---");
   next();
 };
 
